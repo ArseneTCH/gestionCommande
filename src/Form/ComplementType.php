cf33f8bd10e80be933a442bdea4a62d22b3cc2a8
Arsene <lidaw-arsene.tchendo@ism.edu.sn>
2023-01-25 20:35:47 +0000

    1st part

---
+++ src/Form/ComplementType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Complement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ComplementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            
+            ->add('nom', TextType::class, ['label' => false,'required' => false])
+            ->add('prix', TextType::class, ['label' => false,'required' => false,])
+            ->add('type', choiceType::class, ['label' => false,
+            'mapped' => false,
+            'required' => false,
+            'choices' => [
+                    'Boisson' => 'boisson' ,
+                    'Fritte' =>'fritte' ,
+                   
+                ],
+            
+            ])
+            ->add('image', FileType::class, [
+                'label' => false,
+                'multiple' => true,
+                'mapped' => false,
+                'required' => false
+            ])
+           
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Complement::class,
+        ]);
+    }
+}
